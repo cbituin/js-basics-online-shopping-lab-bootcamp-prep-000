b7b9427c4881c2207a4adf032828bee7000a58de
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-03 22:33:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var cart = [];
 
 function getCart() {
- return cart;
+    return cart;
 }
 
 function setCart(c) {
-  cart = c;
-  return cart;
+    cart = c;
+    return cart;
 }
 
 // Instructions
@@ -18,7 +18,7 @@ function setCart(c) {
 // HINT: Look into Math.random() and Math.floor().
 // Upon the successful addition of a new item to the cart, the function should return <itemName> has been added to your cart. .
 
-function addToCart(item){
+function addToCart(item) {
     item = {
         itemName: item,
         itemPrice: Math.floor((Math.random() * 10) + 1)
@@ -36,26 +36,25 @@ function addToCart(item){
 // 3+ items — In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.
 
 function viewCart() {
-    // if(cart.length <= 0){
-    //     return 'Your shopping cart is empty.'
-    // } else {
-    //     let cartStart = "In your cart, you have ",
-    //         cartArr = [];
-    //         cartEnd = " and ${cart[i].sli.itemName}
-    //     for(let i = 0; i < cart.length - 1; i++){
-    //         cartArr.push('${cart[i].itemName} at ${cart[i].itemPrice}');
-    //     }
-    //     return cartStart + cartArr + cartEnd;
-    // }
+    if (cart !== undefined){
+        for(let i = 0; i < cart.length; i++){
+            console.log('hi')
+        }
+    } else{
+        return 'Your shopping cart is empty.'
+    }
 }
 
 
 //TODO: The total() function accepts no arguments, iterates through the cart array, and returns the current total value of the items in the cart.
 
 function total() {
-    // write your code here
-  }
-  
+    let totalPrice = 0;
+    for (let i = 0; i < cart.length; i++) {
+        totalPrice += parseInt(cart[i].itemPrice);
+    }
+    return totalPrice;
+}
 
 //TODO: The removeFromCart() function accepts one argument, the name of the item that should be removed.
 // If the item is present in the cart, the function should remove the object from the cart and then return the updated cart.
@@ -63,8 +62,16 @@ function total() {
 // If the cart does not contain a matching item, the function should return That item is not in your cart.
 
 function removeFromCart(item) {
-    // write your code here
+    for(let i = 0; i < getCart().length; i++){
+        if(item != getCart()[i]){
+          return 'That item is not in your cart.';
+        } else{
+          getCart().splice(i, 1);
+          getCart();
+        }
+    }
   }
+
 
 //TODO: The placeOrder() function accepts one argument, a credit card number.
 // If no argument is received, the function should print out Sorry, we don't have a credit card on file for you..
@@ -73,6 +80,11 @@ function removeFromCart(item) {
 // return Your total cost is $71, which will be charged to the card 83296759. (where 71 is the value returned by total() and 83296759 is the credit card number passed to placeOrder())
 
 function placeOrder(cardNumber) {
-    // write your code here
-  }
-  
+    if(cardNumber === undefined){
+        return "Sorry, we don't have a credit card on file for you.";
+    } else {
+        cart = [];
+        let cartTotal = total();
+        return 'Your total cost is $' + cartTotal + ', which will be charged to the card ' + cardNumber + '.';
+    }
+}
